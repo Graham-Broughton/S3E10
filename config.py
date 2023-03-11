8db8c40e65bcb3a5612925075be7213d3438b4fe
Graham-Broughton <broughton.graham@gmail.com>
2023-03-11 04:25:56 -0800

    initial commit

---
+++ config.py
@@ -0,0 +1,18 @@
+import os
+from dataclasses import dataclass
+
+BASE_PATH = os.path.dirname(os.path.abspath(__file__))
+DATA_PATH = os.path.join(BASE_PATH, "data")
+RAW_DATA = os.path.join(DATA_PATH, "raw")
+PROCESSED_DATA = os.path.join(DATA_PATH, "processed")
+
+
+@dataclass
+class CFG:
+    NROWS: int = 100
+    NCOLS: int = 30
+    BASE_PATH: str = BASE_PATH
+    DATA_PATH: str = DATA_PATH
+    RAW_DATA: str = RAW_DATA
+    PROCESSED_DATA: str = PROCESSED_DATA
+    
